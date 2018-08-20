343e417f752c909126113ff793207457a1e00bb3
Aki Gao <iknowsmtp@gmail.com>
2018-08-19 21:09:07 -0400

    Add passport authentication strategy and serialization logic

---
+++ redirects/passport.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+
+passport.use(
+  'local',
+  new localStrategy((username, password, done) => {
+    if (username !== 'user' || password !== 'password') {
+      return done(null, false, {
+        message: 'username or password is incorrect',
+      });
+    }
+    return done(
+      null,
+      { username, password },
+      { message: 'authentication successful!' },
+    );
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
